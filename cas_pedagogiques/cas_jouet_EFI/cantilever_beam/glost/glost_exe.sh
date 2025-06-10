7b148ac798e6e1d1634858abf7bbe87113dd497b
Elias Fekhari <elias.fekhari@gmail.com>
2025-06-10 12:00:05 +0200

    Removes outdated cas_pedagogiques folder

--- cas_pedagogiques/cas_jouet_EFI/cantilever_beam/glost/glost_exe.sh
+++
@@ -1,5 +0,0 @@
-
-#!/bin/bash
-
-cd /fscronos/scratch/users/g88077/uqot/hpc/cas_pedagogiques/cas_jouet_EFI/cantilever_beam/results_09-10-2024_10-56/SIMU_$1
-/fscronos/scratch/users/g88077/uqot/hpc/cas_pedagogiques/cas_jouet_EFI/cantilever_beam/template/beam -x beam_input.xml
--- cas_pedagogiques/cas_jouet_PBL/lib_py/myLib/myLib/__init__.py
+++
@@ -1,1 +0,0 @@
-from .toolBox import *
